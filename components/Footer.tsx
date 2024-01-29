fa70cace52d24560a571e6b3d2118003a75c29a2
Yuliia <yu6785963@gmail.com>
2024-01-29 11:16:38 +0200

    fix copyright

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className='mb-10 px-4 flex flex-col items-center justify-center text-center text-gray-500'>
       <small className='mb-2 text-xs block'>
-        &copy; 2024 Yuliia. All rights reserved.
+        © 2024 Yuliia. All rights reserved.
       </small>
       <p className='text-xs'>
         <span className='font-semibold'>About this website</span> built with React, Next.js, TypeScript, Tailwind CSS, Framer Motion, Vercel hosting.
